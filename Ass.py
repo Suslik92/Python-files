1951ed6d1ab3684c9d1c765a142f17a2b0c0a947
Suslik92 <87718785+Suslik92@users.noreply.github.com>
2021-10-21 22:43:31 +0300

    Update Ass.py

--- Ass.py
+++ Ass.py
@@ -2,13 +2,31 @@
 from time import sleep
 from transitions import Machine
 import logging
+import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
 logging.basicConfig(filename='Log.txt',filemode='a',
                     level=logging.info,format='%(asctime)s -- %(message)s',
                     datefmt='%d/%m/%Y %H:%M:%S')
 
 def Log_Event(Event_MSG) #Logs Event_MSG to Log.txt upon request
     logging.info('Event_MSG')
+
+def Authorize_Ignition() #Opens the relay
+    GPIO.setup(2, GPIO.OUT)
+    GPIO.output(2, GPIO.HIGH)
+
+def Take_Alcohol_Reading()
+    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Setup the default pin for input
+    GPIO.setup(8, GPIO.OUT)
+    Positive_Input = 0
+    For i in range (2): #Take 3 readings 2 secs apart
+        if (~GPIO.Input(18)):
+            Positive_Input +=1
+            time.sleep(2)
+    if (Positive_Input >= 2): return 1 #Return 1 for abnormal values
+    else: return 0 #Return 0 for normal values
+
 
 class Matter(object):
     def say_hello(self): print("hello, new state!")
@@ -17,6 +35,8 @@ class Matter(object):
          print("We've just entered Start")
          Log_Event('System Started') #Log the bootup
          #Display "calibrating sensors via tkinter"
+         #after 10s
+
 
 
 SBS = Matter()
@@ -54,5 +74,7 @@ machine = Machine(SBS, states=states, transitions = transitions, initial='Initia
 #lump.sublimate()
 
 SBS.TrigToStart()
-
+SBS.System_Ready()
+Alc_Flag = Take_Alcohol_Reading()
+SBS.Measurement_Complete()
 print (SBS.state)
