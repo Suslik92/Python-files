9883d1806e026d0211fb113181e80027fd002c79
Suslik92 <87718785+Suslik92@users.noreply.github.com>
2021-10-21 10:52:24 +0300

    Changed some shit

--- Ass.py
+++ Ass.py
@@ -1,30 +1,31 @@
 #from picamera import PiCamera
 from time import sleep
 from transitions import Machine
-import random
+import logging
 
 class Matter(object):
     def say_hello(self): print("hello, new state!")
     def say_goodbye(self): print("goodbye, old state!")
-    def on_enter_Start(self): print("We've just entered state A!")
+    def on_enter_Start(self):
+         print("We've just entered Start")
 
-lump = Matter()
+SBS = Matter()
 
 # Same states as above, but now we give StateA an exit callback
-states = ['Start', 'Initial', 'Measurement', 'Verification', 'Passed', 'Failed', 'Awaiting_Reply']
+states = ['Initial', 'Start', 'Measurement', 'Verification', 'Passed', 'Failed', 'Awaiting_Reply']
 
 transitions = [
     { 'trigger': 'TrigToStart', 'source': 'Initial', 'dest': 'Start' },
     { 'trigger': 'System_Ready', 'source': 'Initial', 'dest': 'Measurement' },
     { 'trigger': 'Measurement_Complete', 'source': 'Measurement', 'dest': 'Verification' },
     { 'trigger': 'Verification_Successful', 'source': 'Verification', 'dest': 'Passed' },
-    { 'trigger': 'Verification_Failed', 'source': 'Verification', 'dest': 'Failed' }
+    { 'trigger': 'Verification_Failed', 'source': 'Verification', 'dest': 'Failed' },
     { 'trigger': 'SMS_Sent', 'source': 'Failed', 'dest': 'Awaiting_Reply' }
     #Success to restart
     #Failed to restart
 ]
 
-machine = Machine(lump, states=states, transitions = transitions, initial='Initial')
+machine = Machine(SBS, states=states, transitions = transitions, initial='Initial')
 
 # Callbacks can also be added after initialization using
 # the dynamically added on_enter_ and on_exit_ methods.
@@ -36,5 +37,6 @@ machine = Machine(lump, states=states, transitions = transitions, initial='Initi
 #machine.set_state('solid')
 #lump.sublimate()
 
-lump.melt()
-print (lump.state)
+SBS.TrigToStart()
+
+print (SBS.state)
